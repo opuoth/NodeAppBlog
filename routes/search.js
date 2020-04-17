22b3ba6dbc17e06aeffdfe9ed45dd5aa460dc1d0
Keisuke Nakajima <keisukewithaaa@gmail.com>
2020-04-17 15:55:42 +0900

    add pagination

--- routes/search.js
+++ routes/search.js
@@ -1,20 +1,39 @@
 var { CONNECTION_URL, OPTIONS, DATABSE } = require("../config/mongodb.config.js");
+var { MAX_ITEM_PER_PAGE } = require("../config/app.config").search;
 var router = require("express").Router();
 var MongoClient = require("mongodb").MongoClient;
 
 router.get("/*", (req, res) => {
+  var page = req.query.page ? parseInt(req.query.page) : 1;
   var keyword = req.query.keyword || "";
 
   var regexp = new RegExp(`.*${keyword}.*`);
+  var query = {
+    $or: [{ title: regexp }, { content: regexp }]
+  };
 
   MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
     var db = client.db(DATABSE);
-    db.collection("posts").find({
-      $or: [{ title: regexp }, { content: regexp }]
-    }).sort({ published: -1 }).toArray().then((list) => {
+
+    Promise.all([
+      db.collection("posts")
+        .find(query)
+        .count(),
+      db.collection("posts")
+        .find(query)
+        .sort({ published: -1 })
+        .skip( (page - 1) * MAX_ITEM_PER_PAGE)
+        .limit(MAX_ITEM_PER_PAGE)
+        .toArray()
+    ]).then((results) => {
       var data = {
         keyword,
-        list
+        count: results[0],
+        list: results[1],
+        pagination: {
+          max: Math.ceil(results[0] / MAX_ITEM_PER_PAGE),
+          current: page
+        }
       };
       res.render("./search/list.ejs", data);
     }).catch((error) => {
