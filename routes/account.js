d1630fc69964da847ce2ac0aa6a1efabac9c9317
Keisuke Nakajima <keisukewithaaa@gmail.com>
2020-04-22 00:52:17 +0900

    logout

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,5 @@
 var { CONNECTION_URL, OPTIONS, DATABSE } = require("../config/mongodb.config");
-var { authenticate } = require("../lib/security/accountcontrol.js");
+var { authenticate, authorize } = require("../lib/security/accountcontrol.js");
 var router = require("express").Router();
 var MongoClient = require("mongodb").MongoClient;
 var tokens = new require("csrf")();
@@ -37,13 +37,7 @@ var validateRegistData = function(body) {
   return isValidated ? undefined : errors;
 };
 
-router.get("/", (req, res, next) => {
-  if(req.isAuthenticated()) {
-    next();
-  } else {
-    res.redirect("/account/login");
-  }
-}, (req, res) => {
+router.get("/", authorize("readWrite"), (req, res) => {
   res.render("./account/index.ejs");
 });
 
@@ -55,7 +49,12 @@ router.get("/login", (req, res) => {
 //ログイン処理
 router.post("/login", authenticate());
 
-router.get("/posts/regist", (req, res) => {
+router.post("/logout", (req, res) => {
+  req.logout();
+  res.redirect("/account/login");
+});
+
+router.get("/posts/regist", authorize("readWrite"), (req, res) => {
   tokens.secret((error, secret) => {
     var token = tokens.create(secret);
     req.session._csrf = secret;
@@ -65,13 +64,13 @@ router.get("/posts/regist", (req, res) => {
 });
 
 // 戻るボタン
-router.post("/posts/regist/input", (req, res) => {
+router.post("/posts/regist/input", authorize("readWrite"), (req, res) => {
   var original = createRegistData(req.body);
   res.render("./account/posts/regist-form.ejs", { original });
 });
 
 // 確認ボタン
-router.post("/posts/regist/confirm", (req, res) => {
+router.post("/posts/regist/confirm", authorize("readWrite"), (req, res) => {
   var original = createRegistData(req.body);
   var errors = validateRegistData(req.body);
   if(errors){
@@ -81,7 +80,7 @@ router.post("/posts/regist/confirm", (req, res) => {
   res.render("./account/posts/regist-confirm.ejs", { original });
 });
 
-router.post("/posts/regist/execute", (req, res) => {
+router.post("/posts/regist/execute", authorize("readWrite"), (req, res) => {
   var secret = req.session._csrf;
   var token = req.cookies._csrf;
   
@@ -113,7 +112,7 @@ router.post("/posts/regist/execute", (req, res) => {
 });
 
 //登録完了画面
-router.get("/posts/regist/complete", (req, res) => {
+router.get("/posts/regist/complete", authorize("readWrite"), (req, res) => {
   res.render("./account/posts/regist-complete.ejs");
 });
 
