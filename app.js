9888fa9e7e25d692d371c881db8a0aedbc0a288f
Keisuke Nakajima <keisukewithaaa@gmail.com>
2020-04-23 01:07:41 +0900

    custom header

--- app.js
+++ app.js
@@ -30,15 +30,23 @@ app.use(bodyParser.json());
 app.use(flash());
 app.use(...accountcontrol.initialize());
 
-app.use("/", require("./routes/index.js"));
-
-app.use("/posts/", require("./routes/posts.js"));
-
-app.use("/search/", require("./routes/search.js"));
-
-app.use("/account/", require("./routes/account.js"));
-
-app.use("/api/posts", require("./api/posts.js"))
+app.use("/api", (()=>{
+  var router = express.Router();
+  router.use("/posts", require("./api/posts.js"));
+  return router;
+})());
+app.use("/", (()=>{
+  var router = express.Router();
+  router.use((req, res, next) => {
+    res.setHeader("X-Frame-Options", "SAMEORIGIN");
+    next();
+  });
+  router.use("/posts/", require("./routes/posts.js"));
+  router.use("/search/", require("./routes/search.js"));
+  router.use("/account/", require("./routes/account.js"));
+  router.use("/", require("./routes/index.js"));
+  return router;
+})());
 
 app.use(systemlogger());
 
