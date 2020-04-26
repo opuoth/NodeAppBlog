3ee6b319031741632f649535fffbc113652f76c8
Keisuke Nakajima <keisukewithaaa@gmail.com>
2020-04-26 16:43:55 +0900

    add User index

--- app.js
+++ app.js
@@ -42,45 +42,46 @@ app.use("/", (() => {
   router.use("/posts/", require("./routes/posts.js"));
   router.use("/search/", require("./routes/search.js"));
   router.use("/account/", require("./routes/account.js"));
+  router.use("/users/", require("./routes/users.js"));
   router.use("/", require("./routes/index.js"));
   return router;
 })());
 
 app.use(systemlogger());
 
-app.use((req, res, next) => {
-  var data = {
-    method: req.method,
-    protocol: req.protocol,
-    version: req.httpVersion,
-    url: req.url
-  };
-  res.status(404);
-  if (req.xhr) {
-    res.json(data);
-  } else {
-    res.render("./404.ejs", { data });
-  }
-});
+// app.use((req, res, next) => {
+//   var data = {
+//     method: req.method,
+//     protocol: req.protocol,
+//     version: req.httpVersion,
+//     url: req.url
+//   };
+//   res.status(404);
+//   if (req.xhr) {
+//     res.json(data);
+//   } else {
+//     res.render("./404.ejs", { data });
+//   }
+// });
 
-app.use((err, req, res, next) => {
-  var data = {
-    method: req.method,
-    protocol: req.protocol,
-    version: req.httpVersion,
-    url: req.url,
-    error: (process.env.NODE_ENV === "development") ? {
-      name: err.name,
-      message: err.message,
-      stack: err.stack
-    } : undefined
-  };
-  res.status(500);
-  if (req.xhr) {
-    res.json(data);
-  } else {
-    res.render("./500.ejs", { data });
-  }
-});
+// app.use((err, req, res, next) => {
+//   var data = {
+//     method: req.method,
+//     protocol: req.protocol,
+//     version: req.httpVersion,
+//     url: req.url,
+//     error: (process.env.NODE_ENV === "development") ? {
+//       name: err.name,
+//       message: err.message,
+//       stack: err.stack
+//     } : undefined
+//   };
+//   res.status(500);
+//   if (req.xhr) {
+//     res.json(data);
+//   } else {
+//     res.render("./500.ejs", { data });
+//   }
+// });
 
 app.listen(3000);
---
+++ routes/users.js
@@ -0,0 +1,71 @@
+var { CONNECTION_URL, OPTIONS, DATABSE } = require("../config/mongodb.config");
+var { authenticate, authorize } = require("../lib/security/accountcontrol.js");
+var { MAX_ITEM_PER_PAGE } = require("../config/app.config").search;
+var router = require("express").Router();
+var MongoClient = require("mongodb").MongoClient;
+
+router.get("/index", (req, res) => {
+  var page = req.query.page ? parseInt(req.query.page) : 1;
+  var keyword = req.query.keyword || "";
+
+  var regexp = new RegExp(`.*${keyword}.*`);
+  var query = {
+    name: regexp
+  };
+  MongoClient.connect(CONNECTION_URL, OPTIONS, (error, client) => {
+    var db = client.db(DATABSE);
+    Promise.all([
+      db.collection("users")
+        .find(query)
+        .count(),
+      db.collection("users")
+        .find(query)
+        .sort({ name: 1 })
+        .skip( (page - 1) * MAX_ITEM_PER_PAGE)
+        .limit(MAX_ITEM_PER_PAGE)
+        .toArray()
+    ]).then((results) => {
+      var data = {
+        keyword,
+        count: results[0],
+        users: results[1],
+        pagination: {
+          max: Math.ceil(results[0] / MAX_ITEM_PER_PAGE),
+          current: page
+        }
+      };
+      res.render("./users/list.ejs", data);
+    }).catch((error) => {
+      console.log(query);
+      throw error;
+    }).then(() => {
+      client.close();
+    });
+  });
+});
+
+router.get("/:email", (req, res) => {
+  var email = req.params.email;
+  MongoClient.connect( CONNECTION_URL, OPTIONS, (error, client) => {
+    var db = client.db(DATABSE);
+  
+    db.collection("users")
+      .findOne({email: email}
+    )
+    .then((user)=>{
+      db.collection("posts")
+        .find({authors: [user.name]})
+        .sort({ published: -1 })
+        .toArray((error, posts) =>{
+          var data = {user: user, list: posts};
+          res.render("./users/show.ejs", data);
+        })
+    }).catch((error)=>{
+      throw error;
+    }).then(()=>{
+      client.close();
+    });
+  });
+});
+
+module.exports = router;
